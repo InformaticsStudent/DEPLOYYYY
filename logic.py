1633adc2f16a8cb0a588d8b52e4ad9703f15cf09
rikiefendi2 <rikie38187@gmail.com>
2026-05-14 02:02:28 +0700

    clean build

--- logic.py
+++
@@ -1,8 +0,0 @@
-MKI = {
-    "MKI-001": { #kode unik ya inituh intinya
-        "judul": "AI Ethics Governance",
-        "leader": "Melvin Rahma Sayuga Subroto, S.Pd., M.Acc.",
-        "anggota": 48,
-        "status": "Aktif"
-    }
-}
--- tugas struktur data.py
+++
@@ -1,7 +0,0 @@
-akses_server = {"Andi", "Budi", "Citra", "Doni"}
-akses_db = {"Budi", "Doni", "Eka", "Fajar"}
-akses_api = {"Citra", "Doni", "Fajar", "Gilang"}
-
-#1 BARIS CODE PAKAI |
-hasil = ((akses_server & akses_db) | (akses_db & akses_api) | (akses_server & akses_api)) - (akses_server & akses_db & akses_api)
-print(hasil)
